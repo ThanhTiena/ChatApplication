e8e5160f1d2d096aec30ea5875c78649c5a718f7
ThanhTiena <86332664+ThanhTiena@users.noreply.github.com>
2022-07-14 20:59:43 +0700

    test branch

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,4 +1,9 @@
 package Application;
 
+import Models.User;
+
 public class Application {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Models/User.java
+++ src/Models/User.java
@@ -3,4 +3,9 @@ package Models;
 public class User {
     String Name;
     String Password;
+
+    public User(String name, String password) {
+        this.Name = name;
+        this.Password = password;
+    }
 }
