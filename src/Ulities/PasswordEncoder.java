26d8556d20ad604894d0e8939e7474dabd7a212a
ThanhTiena <86332664+ThanhTiena@users.noreply.github.com>
2022-07-17 11:53:56 +0700

    Add Bcryt Jar

--- src/Ulities/PasswordEncoder.java
+++ src/Ulities/PasswordEncoder.java
@@ -1,16 +1,18 @@
 package Ulities;
 
+import org.mindrot.BCrypt;
+
 public class PasswordEncoder {
 
-//    public String hashPassword(String plainTextPassword) {
-//        String salt = BCrypt.gensalt();
-//        return BCrypt.hashpw(plainTextPassword, salt);
-//    }
-//
-//    public boolean checkPassword(String plainTextPassword, String hashedPassword) {
-//        if (null == hashedPassword || !hashedPassword.startsWith("$2a$")) {
-//            throw new RuntimeException("Hashed password is invalid");
-//        }
-//        return BCrypt.checkpw(plainTextPassword, hashedPassword);
-//    }
+    public String hashPassword(String plainTextPassword) {
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(plainTextPassword, salt);
+    }
+
+    public boolean checkPassword(String plainTextPassword, String hashedPassword) {
+        if (null == hashedPassword || !hashedPassword.startsWith("$2a$")) {
+            throw new RuntimeException("Hashed password is invalid");
+        }
+        return BCrypt.checkpw(plainTextPassword, hashedPassword);
+    }
 }
