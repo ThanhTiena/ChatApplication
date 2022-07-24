e3d070fb018072c6d86ce96cb35b1e36b324349b
ThanhTiena <86332664+ThanhTiena@users.noreply.github.com>
2022-07-24 22:16:07 +0700

    Add Enum Class

--- src/main/Models/Group.java
+++ src/main/Models/Group.java
@@ -1,13 +1,14 @@
 package main.Models;
 
+import main.Models.Enums.GroupType;
+
 import java.util.HashMap;
 import java.util.*;
 
 public abstract class Group {
     private Map<String, Object> members = new HashMap<>();
-
-    /*
-    * Methods below
-    * */
+    private Map<String, User> admins = new HashMap<>();
+    private GroupType groupType;
+    private String accessCode;
 
 }
