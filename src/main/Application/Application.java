398fa73ca2fbc55018e1c9bae52e44bf75367735
danhdang166 <dangthanhdanh166@gmail.com>
2022-07-30 09:50:02 +0700

    add user interface menu

--- src/main/Application/Application.java
+++ src/main/Application/Application.java
@@ -13,6 +13,6 @@ import java.util.*;
 public class Application {
     public static void main(String[] args) {
         ChatMediator chatMediator = new ChatMediator();
-
+        chatMediator.doTask();
     }
 }
--- src/main/Application/ChatMediator.java
+++ src/main/Application/ChatMediator.java
@@ -6,22 +6,51 @@ import java.util.Scanner;
 
 public class ChatMediator {
     private DataStorage dataStorage;
-
+    private Scanner scanner = new Scanner(System.in);
     public ChatMediator(){
         dataStorage = DataStorage.getInstance();
     }
 // Danh lam
     public void openConversation(){
-        Scanner scanner = new Scanner(System.in);
-    }
 
-    private String getUserInterface(){
+    }
+    private void getUserInterface(){
         String userInterface;
-        userInterface = "########################";
-        userInterface += "1. Login";
-        userInterface += "2. Logout";
-        userInterface += "3. Open Chat";
-        userInterface += "#######################";
-        return userInterface;
+        userInterface = "######################## \n";
+        userInterface += "1. Login \n";
+        userInterface += "2. Logout \n";
+        userInterface += "3. Open Chat \n";
+        userInterface += "####################### \n";
+        userInterface += "Your choice: ";
+        System.out.println(userInterface);
+    }
+    public void doTask() {
+        int choice;
+        do {
+            getUserInterface();
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    doLogin();
+                    break;
+                case 2:
+                    doLogout();
+                    break;
+                case 3:
+                    doOpenChat();
+                    break;
+                default:
+                    System.out.println("Wrong input");
+            }
+        } while (choice != 3);
+    }
+    private void doLogin() {
+        System.out.println("Login is loading");
+    }
+    private void doLogout() {
+        System.out.println("Logout is loading");
+    }
+    private void doOpenChat() {
+        System.out.println("OpenChat is loading");
     }
 }
