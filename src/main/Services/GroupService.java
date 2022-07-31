ad86ae0cef8b78e8ebca59dcb39efe48974c87c7
Nguyễn Hữu Nhân <85977626+nhanng123@users.noreply.github.com>
2022-07-31 23:19:22 +0700

    Update atttribute groupType to type String, add Find Friend method and create group chat method

--- src/main/Services/GroupService.java
+++ src/main/Services/GroupService.java
@@ -1,14 +1,43 @@
 package main.Services;
 
 import main.Data.DataStorage;
+import main.Models.Enums.GroupType;
 import main.Models.Group;
+import main.Models.PrivateGroup;
+import main.Models.PublicGroup;
+import main.Models.User;
+import main.Ulities.GenerateNumber;
+
+import java.util.ArrayList;
 
 public class GroupService {
     private DataStorage dataStorage;
     private Group group;
-
     public GroupService(){
+
         this.dataStorage = DataStorage.getInstance();
     }
 
+    public boolean createGroupChat(ArrayList<User> memberList, User admin, String groupName, String groupType) throws Exception{
+        try{
+
+            if(groupType == "Private"){
+              PrivateGroup  newGroup = new PrivateGroup(memberList,admin,groupName,groupType,GenerateNumber.generateGroupId(),"active");
+                dataStorage.groups.insert(newGroup);
+            }
+            else{
+                if(groupType == "Public"){
+                   PublicGroup newGroup = new PublicGroup(memberList,admin,groupName,groupType,GenerateNumber.generateGroupId(),"active");
+                   dataStorage.groups.insert(newGroup);
+                }
+            }
+
+            return true;
+
+        }
+        catch (Exception ex) {
+            ex.getMessage();
+            return false;
+        }
+    }
 }
